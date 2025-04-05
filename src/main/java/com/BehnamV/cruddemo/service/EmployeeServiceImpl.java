9e1da12347257a1a20e094762a04912bf7b0eba9
Behnam <behnam.vosoogh@outlook.com>
2025-04-05 18:29:05 -0400

    find employee by id added

--- src/main/java/com/BehnamV/cruddemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/BehnamV/cruddemo/service/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package com.BehnamV.cruddemo.service;
 import com.BehnamV.cruddemo.dao.EmployeeDAO;
 import com.BehnamV.cruddemo.entity.Employee;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 @Service
@@ -14,5 +15,20 @@ public class EmployeeServiceImpl implements EmployeeService{
     @Override
     public List<Employee> findAll() {
         return employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(int theId) {
+        return employeeDAO.findById(theId);
+    }
+    @Transactional
+    @Override
+    public Employee save(Employee theEmployee) {
+        return employeeDAO.save(theEmployee);
+    }
+    @Transactional
+    @Override
+    public void deleteById(int theId) {
+        employeeDAO.deleteById(theId);
     }
 }
